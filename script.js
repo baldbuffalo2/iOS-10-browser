ce4c7fbffb460cd70bcd9a23bd7756d4c7e00a5b
baldbuffalo2 <111616558+baldbuffalo2@users.noreply.github.com>
2025-01-13 02:04:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.getElementById("loadBtn").addEventListener("click", function() {
-  let url = document.getElementById("urlInput").value.trim(); // Use let instead of const
+  let url = document.getElementById("urlInput").value.trim(); // Use let here
   const iframe = document.getElementById("browserFrame");
 
   // Add 'http://' if the URL doesn't include a scheme
   if (!/^https?:\/\//i.test(url)) {
-    url = 'http://' + url; // Reassigning url here is fine since it's let
+    url = 'http://' + url; // This is fine, since it's let
   }
 
   // Validate the URL
   try {
-    new URL(url); // This will throw an error if the URL is invalid
+    new URL(url); // Validate the URL
     iframe.src = url;
   } catch (e) {
     showAlert("Please enter a valid URL.");
