67a3e7042414a018a14c9aaf3bdc2ac965e8457e
baldbuffalo2 <111616558+baldbuffalo2@users.noreply.github.com>
2025-01-13 01:44:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.getElementById("loadBtn").addEventListener("click", function() {
-  const url = document.getElementById("urlInput").value;
+  const url = document.getElementById("urlInput").value.trim();
   const iframe = document.getElementById("browserFrame");
 
-  if (url) {
+  // Add 'http://' if the URL doesn't include a scheme
+  if (!/^https?:\/\//i.test(url)) {
+    url = 'http://' + url;
+  }
+
+  // Validate the URL
+  try {
+    new URL(url); // This will throw an error if the URL is invalid
     iframe.src = url;
-  } else {
+  } catch (e) {
     alert("Please enter a valid URL.");
   }
 });
